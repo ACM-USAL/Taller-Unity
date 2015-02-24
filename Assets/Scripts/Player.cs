91723d73dc291834def2d8305252e8adb9b30aee
Alexandra <alexandra15@usal.es>
2015-02-24 17:10:41 +0100

    Cuando os pongais, haced una rama nueva o un fork
    Pasado el Script de movimiento a C#. Unificados los dos niveles.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,14 +2,20 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
+    public float speed = 1.0f;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    private RaycastHit hit;
+    private Vector3 move;
+
 	// Update is called once per frame
 	void Update () {
-	
+	    move = new Vector3(Input.GetAxis("Horizontal"), 10*Input.GetAxis("Vertical"),0);
+        transform.position += move * speed * Time.deltaTime;
+
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit))
+        {
+            Vector3 objectForward = transform.InverseTransformDirection(Vector3.forward);
+            transform.rotation = Quaternion.LookRotation(objectForward, hit.normal);
+        }
 	}
 }
