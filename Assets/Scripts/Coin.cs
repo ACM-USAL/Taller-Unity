0ec7baa2561c4af33c5b8bc6fc07b42e4e26f06c
Linkaynn <jroarbelo@gmail.com>
2015-02-25 18:49:28 +0100

    Cambios en la descripción
    Añadido colisiones y GUI de vida y score.
    Añadido provisionalmente daño hacia el player de los enemigos.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Coin : MonoBehaviour {
 
+	private GameObject scoreGUI;
+
+	private int score;
+
 	// Use this for initialization
 	void Start () {
-	
+		score = 0;
+		scoreGUI = GameObject.FindWithTag("Score");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		scoreGUI.guiText.text = "Score: " + score;
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Coin") {
+			score += 10;
+			Destroy(other.gameObject);
+		}
 	}
 }
