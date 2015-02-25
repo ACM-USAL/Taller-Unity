a4dc5ac24fdb56045f61b213b0da9d7185e9dc2a
javiglesias <javiglesias18@gmail.com>
2015-02-25 12:46:37 +0100

    Mov Enemigos
    Añadido script de movimiento para enemigos.
    Funciona poniendole los tag "ColisionableIzq" o "ColisionableDer" a los
    elementos con los que podria colisionar por la derecha y por la izq, es
    decir muros.

--- Assets/Scripts/Live.cs
+++ Assets/Scripts/Live.cs
@@ -4,9 +4,18 @@ using System.Collections;
 public class Live : MonoBehaviour {
 	private Vector3 move;
 	public float speed;
+    private int n = -1;
+    //public GameObject objetoColisionable;
 	// Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "ColisionableIzq")
+            n = 1;
+        if (collider.gameObject.tag == "ColisionableDer")
+            n = -1;
+    }
 	void Update () {
-		move = new Vector3(-1,0,0);
+		move = new Vector3(n,0,0);
 		transform.position += move * speed * Time.deltaTime;
 	}
 }
