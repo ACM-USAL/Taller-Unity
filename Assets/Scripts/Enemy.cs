77208b33363133cee62aa5185ce773fe6f60f371
Alexandra <alexandra15@usal.es>
2015-03-03 18:08:26 +0100

    Cambios de esta mañana en la cafetería.
    Sólo falta mejorar el tema de las caídas y añadir los corazones al matar
    enemigos. Ya se pueden hacer escenarios.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,15 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private Vector3 move;
+    public float speed;
+    private int n = 1;
+    private float alpha = 0;
+
+    void Update()
+    {
+        move = new Vector3(n, 0, 0);
+        transform.position += move * speed * Time.deltaTime;        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemycollider")
+        {            
+            ChangeDirection();
+        }
+    }
+
+    public void ChangeDirection()
+    {
+        n *= -1;
+        alpha = (alpha == 180) ? 0 : 180;
+        transform.rotation = new Quaternion(0, alpha, 0, 0);
+    }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,13 +3,63 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
+    private GameObject scoreGUI; //Texto con la puntuación
+    public int score; //Puntuación (Monedas)
+    private GUITexture coin; //Textura para representar las monedas del jugador
+
+    public int nlifes = 3; //Número de vidas iniciales del jugador 
+    private GUITexture[] lifes; //Texturas para representar las vidas del jugador
+
+    public static GameController instance = null;
+    private int level;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        score = 0;
+        scoreGUI = GameObject.FindWithTag("Score");        
+
+        lifes = GameObject.FindWithTag("GuiLife").GetComponentsInChildren<GUITexture>();
+        coin = GameObject.FindWithTag("GuiCoin").GetComponentInChildren<GUITexture>();
+        level = 0;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        scoreGUI.guiText.text = "" + score;                 
 	}
+
+    public void ChangeLifes(bool a)
+    {
+        if (nlifes == 0)
+            return;
+        if (a)
+        {
+            lifes[nlifes].enabled = a;
+            nlifes++;
+        }
+        else
+        {
+            lifes[nlifes - 1].enabled = a;
+            nlifes--;
+        }
+    }
+
+    public void ChangeLevel()
+    {
+        //print("Cargando Level1");
+        level++;
+        Application.LoadLevel(level);
+    }
 }
