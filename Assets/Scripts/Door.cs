523a2fd4fb7d136b9ecf19dbcd5c7c9910876893
Javier Iglesias <javiglesias18@gmail.com>
2015-03-07 16:51:32 +0100

    Nuevo Nivel añadido falta
    Falta que le ponga las layers a cada cosa.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,15 +2,14 @@
 using System.Collections;
 
 public class Door : MonoBehaviour {
-
-	
+    public string nivel; 	
 	// Update is called once per frame
     void OnCollisionEnter2D(Collision2D colision)
     {
         if (colision.gameObject.tag == "Player")
         {
             print("Cargando Level1");
-            Application.LoadLevel("Level1");
+            Application.LoadLevel(nivel);
         }
     }
 }
